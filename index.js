f84bba67564631704dd7ff71aa294b2711e92b62
Vincent Besseau <vbesseau44@gmail.com>
2018-08-25 20:17:10 +0200

    Set module on local only

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = function (sails) {
 
+    var install = require = require('./lib/install.js')
     var exec = require('./lib/playstation4.exec.js');
     var uninstall = require = require('./lib/uninstall.js')
 
